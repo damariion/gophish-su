8a852a2f2b39117ee50b6fa38f960a78686d8d25
Damarion <da.abendanon@gmail.com>
2025-08-13 20:29:32 +0200

    update

---
+++ gen-landings.py
@@ -0,0 +1,42 @@
+from shared.entry import __handle_entry__, Args
+from shared       import env, sep
+# from base64       import b64encode
+
+class Program:
+
+    def __init__(self):
+        pass
+
+    def __main__(self, args: Args):
+        
+        with open(sep.join([env["root"], "assets", 
+            "pages", "landing", args.map["type"] + '.html'])) as page:
+            
+            page = page.read()
+            
+            for holder in ("name", "phone", "email"):
+                page = page.replace(f"{{{{{holder}}}}}", args.map[holder])
+
+        with open(args.map["logo"], 'rb') as logo,\
+             open(args.map["clue"], 'rb') as clue:
+ 
+                ...
+                # page = page.replace(r"{{logo}}", b64encode(logo.read()).decode())
+                # page = page.replace(r"{{clue}}", b64encode(clue.read()).decode())
+
+        page = page.replace(r"{{logox}}", args.map["logo"].split('.')[-1])
+        page = page.replace(r"{{cluex}}", args.map["clue"].split('.')[-1])
+
+        print(page)
+
+__handle_entry__(
+    {__name__: Program},
+    (
+        "name",  # the name of the client
+        "phone", # phone number of the client
+        "email", # email of the client
+        "logo",  # path to an image of the client's logo
+        "clue",  # path to the image of clue(s)
+        "type"   # type of landing-page to be generated
+    )
+)
