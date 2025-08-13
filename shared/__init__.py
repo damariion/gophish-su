63db38d45e1029ce5a3bf433b9660dbd884529b0
Damarion <da.abendanon@gmail.com>
2025-08-13 19:39:44 +0200

    update

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,7 +1,11 @@
+from os import sep
+
 env = {
     "api": {
         "host" : "127.0.0.1",
         "port" : 3333,
         "key"  : open("shared/.key", 'r').read()
-    }
+    },
+
+    "root" : sep.join(__file__.split(sep)[:-2])
 }
--- summary_mail.py
+++ summary_mail.py
@@ -1,41 +1,7 @@
 from datetime     import datetime, timedelta
 from shared.entry import __handle_entry__, Args
 from shared.api   import API
-
-TEMPLATES = {
-    
-    "start": (
-        "De PhishLink {variant}-simulatie van {month} zal zoals gepland {date} van start gaan. Hierbij de details:\n"
-        "\n"
-        "- Om 09:00 uur wordt er om de 4 seconden een e-mail gestuurd. Dat betekent dat rond {last} uur de laatste e-mail verzonden moet zijn. In totaal zijn het namelijk {total} e-mails.\n"
-        "- Morgen zal ik een update geven over hoeveel personen er geklikt hebben na de eerste werkdag;\n"
-        "- Het onderwerp van de mail is '{subject}' en de afzender is '{sender}';\n"
-        "- De simulatie bevat een inlogpagina.\n"
-        "\n"
-        "Ik hoop jullie hiermee voldoende geïnformeerd te hebben en als er nog vragen zijn dan hoor ik het uiteraard graag."
-    ),
-
-    "update": (
-        "Hierbij wil ik een update geven over de statistieken van de phishing e-mail na de eerste werkdag:\n"
-        "\n"
-        "- Er zijn in totaal {sent} e-mails succesvol verzonden.\n"
-        "- In totaal hebben er {clicked} medewerkers ({clicked->%}%) op de link in de phishing e-mail geklikt.\n"
-        "- Tot slot zijn er {submitted} inloggegevens ingevuld ({submitted->%}%).\n"
-        "\n"
-        "Mochten er nog vragen of opmerkingen zijn over de huidige resultaten dan hoor ik het graag."
-    ),
-
-    "end": (
-        "Zojuist heb ik de phishing campagne stopgezet, verdere clicks op de phishing link zullen niet meer geregistreerd worden. "
-        "De eindstand van de simulatie is als volgt:\n"
-        "\n"
-        "- Er zijn in totaal {sent} e-mails succesvol verzonden;\n"
-        "- In totaal hebben {clicked} medewerkers ({clicked->%}%) op de link in de phishing e-mail geklikt;\n"
-        "- Van de klikkende medewerkers hebben {submitted} medewerkers ({clicksubbed->%}%) inloggegevens ingevuld . Dit is {submitted->%}% van het totaal.\n"
-        "\n"
-        "De herkenningspunten heb ik verder bijgevoegd in de bijlagen."
-    )
-}
+from shared       import env, sep
 
 class Program:
 
@@ -82,7 +48,8 @@ class Program:
         
         template = self.api.get(f"campaigns/{args.map['id']}")["template"]
         summary  = self.api.get(f"campaigns/{args.map['id']}/summary") 
-        message  = TEMPLATES[args.map["variant"]]
+        message  = open(sep.join([env["root"], "assets", 
+            "mails", args.map["variant"] + '.txt'])).read()
         
         for key, value in self.assemble(template, summary).items():
             message = message.replace(key, str(value))
