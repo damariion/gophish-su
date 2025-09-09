4d9b0c8094058f4778fb07d8fe7563acb5866146
Damarion <da.abendanon@gmail.com>
2025-09-09 22:47:12 +0200

    update

--- docs/TODO.md
+++ docs/TODO.md
@@ -1,1 +1,2 @@
 * [ ] Microsoft SSO-page generator (with image or default)
+* [ ] Decontextualization tool (removes client names from prompt)
--- filter.py
+++ filter.py
@@ -5,18 +5,31 @@ class Program:
 
     def __init__(self):     
         
-        ...
+        self.emails = []
+        self.syntax = {
+            "opened": "Opened",
+            "clicked": "Clicked Link",
+        }
 
     def __main__(self, args: Args):
         
         with open(args.map["events"], 'r') as file:
             self.data = read_csv(file)
 
-        print(self.data)
+        for event in self.data.values:
+            
+            if  event[3] == args.map["type"] \
+            and event[1] not in self.emails:
+                
+                self.emails.append(event[1])
+                
+        print(*self.emails, sep='\n')
+
 
 __handle_entry__(
     {__name__: Program},
     (
         "events", # exported raw events (from Gophish)
+        "type",   # type of event to filter (clicked, submitted, etc)
     )
 ) 
